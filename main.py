3c2df9a9584ef9dededfc7675e93e216756295bb
Marcel Mann <you@example.com>
2025-04-17 16:26:30 +0200

    changed input

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from storage_json import StorageJson
 from storage_csv import StorageCsv
 from movie_app import MovieApp
 
-storage_options = input("Please choose between storage option (1) for 'json' or (2) for 'csv': ")
 
 def main():
     while True:
+        storage_options = input("Please choose between storage option (1) for 'json' or (2) for 'csv': ")
         if storage_options == "1":
             storage1 = StorageJson("film_database.json")
             movie_app1 = MovieApp(storage1)
             movie_app1.run()
             storage1.create_html()
         if storage_options == "2":
-            storage = StorageCsv("film_database.csv")
-            storage.save_movie()
-            movie_app = MovieApp(storage)
-            movie_app.run()
+            try:
+                storage = StorageCsv("film_database.csv")
+                storage.save_movie()
+                movie_app = MovieApp(storage)
+                movie_app.run()
+            except TypeError:
+                print("csv storage not active. Press (0) to exit.")
+                continue
         elif storage_options != "1" or storage_options != "2":
             break
             # csv storage is not yet activated

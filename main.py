5677863d3eb28fd8318fe25d67438303d77d1269
Marcel Mann <you@example.com>
2025-04-17 11:07:33 +0200

    created user input in main

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from storage_json import StorageJson
 from storage_csv import StorageCsv
 from movie_app import MovieApp
 
+storage_options = input("Please choose between storage option (1) for 'json' or (2) for 'csv': ")
+
 def main():
-    """storage = StorageCsv("film_database.csv")
-    storage.save_movie()
-    movie_app = MovieApp(storage)
-    movie_app.run()"""
-    storage1 = StorageJson("film_database.json")
-    movie_app1 = MovieApp(storage1)
-    movie_app1.run()
-    storage1.create_html()
-    # storage1.add_field_to_movies("runtime", "update movie to add runtime")
+    while True:
+        if storage_options == "1":
+            storage1 = StorageJson("film_database.json")
+            movie_app1 = MovieApp(storage1)
+            movie_app1.run()
+            storage1.create_html()
+        if storage_options == "2":
+            storage = StorageCsv("film_database.csv")
+            storage.save_movie()
+            movie_app = MovieApp(storage)
+            movie_app.run()
+        elif storage_options != "1" or storage_options != "2":
+            break
+            # csv storage is not yet activated
 
 if __name__ == "__main__":
     main()
